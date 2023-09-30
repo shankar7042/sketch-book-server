03072e0195e8e94f8f0fc0ba119ce311f363d619
shankar7042 <shankar.kumar.4171@gmail.com>
2023-09-30 14:52:15 +0530

    whitelist client

--- index.js
+++ index.js
@@ -4,15 +4,20 @@ import { Server } from "socket.io";
 import cors from "cors";
 
 const app = express();
-app.use(cors({ origin: "http://localhost:5173" }));
+const isDev = app.settings.env === "development";
+const URL = isDev
+  ? "http://localhost:5173"
+  : "https://sketch-book-r1ln6d9gk-shankar7042.vercel.app";
+
+app.use(cors({ origin: URL }));
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-  cors: "http://localhost:5173",
+  cors: URL,
 });
 
 io.on("connection", (socket) => {
-  console.log("client connected", socket.id);
+  console.log("client connected");
 
   socket.on("beginPath", (arg) => {
     socket.broadcast.emit("beginPath", arg);
